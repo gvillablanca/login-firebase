6c68ffa399202225d1f59c61ba19ac3aa03a37be
Dany-Nok <daniela_gallardo_m@hotmail.com>
2018-02-19 20:55:04 -0300

    Llamando data de la api con Fetch

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,19 @@
     $('.register').hide();
 
     //evento para ingresar con usuarios registrados
-    $('#send-login').on('click', ()=>{
+    $('#send-login').on('click', () => {
       const email = $('#email-login').val();
       const password = $('#password-login').val();
 
       firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
         // Handle Errors here.
         var errorCode = error.code;
-        var errorMessage = error.message;     
+        var errorMessage = error.message;
       });
       viewer();
     });
 
-    function viewer(){
+    function viewer() {
       firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
           isVisible();
@@ -33,50 +33,66 @@
       });
     }
 
-    function isVisible(){
+    function isVisible() {
       var content = document.getElementById('content');
-      content.innerHTML =  `
+      content.innerHTML = `
 
       <p id="logout-text" >Bienvenido!</p>
-      <button id="logout" class="btn btn-default" onclick="logout()">Cerrar sesión</button> 
+      <button id="logout" class="btn btn-default" onclick="logout()">Cerrar sesión</button>
       `;
     }
 
-    function logout(){
+    function logout() {
       firebase.auth().signOut()
-      .then(function(){
-        console.log('saliendo...');
-        $('#register-login').show();
-        $('#google-login').show();
-        $('#logout').hide();
-        $('#logout-text').hide();
-      })
-      .catch(function (error){
-        console.log(error);
-      })
+        .then(function() {
+          console.log('saliendo...');
+          $('#register-login').show();
+          $('#google-login').show();
+          $('#logout').hide();
+          $('#logout-text').hide();
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
     }
 
-    $('#register-login').on('click', ()=>{
+    $('#register-login').on('click', () => {
       $('.register').show();
       $('.login').hide();
     });
 
     // evento para registrar usuarios
-    $('#send').on('click', ()=>{
+    $('#send').on('click', () => {
       const email2 = $('#email-register').val();
       const password2 = $('#password-register').val();
 
       //  paso 1 registrar usuarios
       firebase.auth().createUserWithEmailAndPassword(email2, password2).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      viewer();
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        viewer();
       });
     });
 
-    $('#home').on('click', ()=>{
+    $('#home').on('click', () => {
       $('.register').hide();
       $('.login').show();
     });
 
+    // Llamando a la data de la Api con "Fetch" (Nota: Envío el código comentado para que no choque, por si acaso...)
+
+    /* $(document).ready(
+
+      fetch('http://yesno.wtf/api/')
+      .then(response => {
+        return response.json(); // Me va a retornar un objeto "response" en formato JSON
+      })
+      .then(data => {
+        console.log('data = ', data); // Pido que me muestre la data en la consola
+      })
+      .catch(error => {
+        console.error('Hay un error...el servidor no quiere pasarte la data >:(');
+      })); // Mensaje de error por si no puedo obtener la data del servidor
+
+    const answer = document.getElementById('answer'); */
